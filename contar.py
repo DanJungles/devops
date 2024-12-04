d678f40c6d7e7d4ab3a1c40c85f2f6015743ee76
DanJungles <Danilojungles@hotmail.com>
2024-12-04 13:06:19 -0300

    Update contar.py

--- contar.py
+++ contar.py
@@ -6,10 +6,9 @@ for i in range(1, 12):
 
 # Simulando uso da dependência 'requests'
 response = requests.get("https://example.com")
-print("Status Code:", response.status_code)
+
 
 # Simulando credenciais para teste de Secret Scanning
 aws_access_key_id = "AKIAFAKEKEY123456789"
 aws_secret_access_key = "FAKesecretKEYexample1234567890abcd1234FAKE"
 
-print("Teste de Secret Scanning Alerts no GitHub.")
