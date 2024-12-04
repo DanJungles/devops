a11b6b771b1c9dd8d407167ad2570a59750adbf6
DanJungles <Danilojungles@hotmail.com>
2024-12-04 12:57:48 -0300

    Update contar.py

--- contar.py
+++ contar.py
@@ -1,2 +1,7 @@
 for i in range(1, 12):
     print(i)
+
+aws_access_key_id = "AKIAFAKEKEY123456789"
+aws_secret_access_key = "FAKesecretKEYexample1234567890abcd1234FAKE"
+
+print("Teste de Secret Scanning Alerts no GitHub.")
