de596e4ed7a2d22eaaa7d7e89833b3f5b2667758
DanJungles <Danilojungles@hotmail.com>
2024-12-04 13:02:29 -0300

    Update contar.py

--- contar.py
+++ contar.py
@@ -1,6 +1,14 @@
+import requests
+
+# Script que conta de 1 a 11
 for i in range(1, 12):
     print(i)
 
+# Simulando uso da dependência 'requests'
+response = requests.get("https://example.com")
+print("Status Code:", response.status_code)
+
+# Simulando credenciais para teste de Secret Scanning
 aws_access_key_id = "AKIAFAKEKEY123456789"
 aws_secret_access_key = "FAKesecretKEYexample1234567890abcd1234FAKE"
 
