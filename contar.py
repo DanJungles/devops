dd5798d27d7630a0ebc3913ec14c8979cdfe6974
DanJungles <Danilojungles@hotmail.com>
2024-12-05 20:13:04 -0300

    deploy na azure function

--- contar.py
+++ contar.py
@@ -1,2 +1,13 @@
-for i in range(1, 11):
+#import requests
+
+# Script que conta de 1 a 11
+for i in range(1, 12):
     print(i)
+
+# Simulando uso da dependência 'requests'
+#response = requests.get("https://example.com")
+
+
+# Simulando credenciais para teste de Secret Scanning
+aws_access_key_id = "AKIAFAKEKEY123456789"
+aws_secret_access_key = "FAKesecretKEYexample1234567890abcd1234FAKE"
