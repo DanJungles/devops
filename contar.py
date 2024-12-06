7daa41d6d3d10782b6e7c71a4ca2e9ac101b79e3
DanJungles <Danilojungles@hotmail.com>
2024-12-05 22:22:58 -0300

    Update contar.py

--- contar.py
+++ contar.py
@@ -1,4 +1,4 @@
-import requests
+#import requests
 import flask
 
 # Script que conta de 1 a 11
@@ -6,10 +6,10 @@ for i in range(1, 12):
     print(i)
 
 # Simulando uso da dependência 'requests'
-response = requests.get("https://example.com")
+#response = requests.get("https://example.com")
 
 
 # Simulando credenciais para teste de Secret Scanning
-aws_access_key_id = "AKIAFAKEKEY123456789"
-aws_secret_access_key = "FAKesecretKEYexample1234567890abcd1234FAKE"
+#aws_access_key_id = "AKIAFAKEKEY123456789"
+#aws_secret_access_key = "FAKesecretKEYexample1234567890abcd1234FAKE"
 
